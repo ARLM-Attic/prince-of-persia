a8681c003a0ad8caa7cdf618b1b976bf6beb362d
SND\polipo_cp <SND\polipo_cp@dd3d63e4-b46e-4461-a8c4-23e16edf1824>
2013-01-30 14:54:30 +0000

    refresh source
    git-svn-id: https://princeofpersia.svn.codeplex.com/svn@23680 dd3d63e4-b46e-4461-a8c4-23e16edf1824 (+1 squashed commits)
    
    Squashed commits:
    
    [afce824] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@23679 dd3d63e4-b46e-4461-a8c4-23e16edf1824

---
+++ PrinceOfPersia.net/PrinceOfPersia.net/utils/SyncList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrinceOfPersia
+{
+    public class SyncList 
+    { 
+        object syncRoot = new object(); 
+        List<Tile> list = new List<Tile>();
+    
+        public void Add(Tile item) 
+        { 
+            lock (syncRoot) list.Add(item);
+        }
+
+        public int Count
+        {
+            get { lock (syncRoot) return list.Count; }
+        }
+
+        public IteratorWrapper GetIteratorWrapper()
+        {
+            return new IteratorWrapper(this);
+        }
+
+    }
+
+    public class IteratorWrapper : IDisposable, IEnumerable<T>
+    {
+        bool disposed;
+        List<Tile> c;
+        
+        public IteratorWrapper(List<Tile> c) 
+        { 
+            this.c = c; 
+            Monitor.Enter(c.syncRoot); 
+        }
+        
+        public void Dispose() 
+        { if (!disposed) Monitor.Exit(c.syncRoot); disposed = true; }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return c.list.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+
+    }
+}

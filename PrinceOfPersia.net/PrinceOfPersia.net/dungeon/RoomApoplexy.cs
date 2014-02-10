8a0a3c8df6613adf37abd4349ef6430aeb86687c
SND\polipo_cp <SND\polipo_cp@dd3d63e4-b46e-4461-a8c4-23e16edf1824>
2014-02-10 16:15:50 +0000

    Final release 2
    git-svn-id: https://princeofpersia.svn.codeplex.com/svn@32726 dd3d63e4-b46e-4461-a8c4-23e16edf1824

---
+++ PrinceOfPersia.net/PrinceOfPersia.net/dungeon/RoomApoplexy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrinceOfPersia
+{
+    public class RoomApoplexy
+    {
+        public string number = "0";
+        private int size = 30;
+        public TileApoplexy[] tiles;
+
+        public RoomApoplexy()
+        {
+            tiles = new TileApoplexy[size];
+            for (int x = 0; x < tiles.Length; x++)
+            {
+                tiles[x] = new TileApoplexy();
+            }
+
+        }
+
+        //public RoomApoplexy(int sizeX)
+        //{
+        //    columns = new Column[sizeX];
+        //    for(int x = 0; x < columns.Length; x++)
+        //    {
+        //        columns[x] = new Column();
+        //    }
+        //}
+
+    }
+}
---
+++ PrinceOfPersia.net/PrinceOfPersia.net/dungeon/TileApoplexy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrinceOfPersia
+{
+    public class TileApoplexy
+    {
+
+        public string element;
+        public string modifier;
+        
+        public TileApoplexy()
+        {
+        }
+
+   
+
+    }
+}

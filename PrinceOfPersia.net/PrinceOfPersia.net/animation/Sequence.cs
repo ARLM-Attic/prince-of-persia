3b1196599c867ac4f0863fd7189ca498b771783f
SND\polipo_cp <SND\polipo_cp@dd3d63e4-b46e-4461-a8c4-23e16edf1824>
2014-08-01 09:38:03 +0000

    apoplexy level xml read
    git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35210 dd3d63e4-b46e-4461-a8c4-23e16edf1824 (+3 squashed commit)
    
    Squashed commit:
    
    [c036d5c] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35199 dd3d63e4-b46e-4461-a8c4-23e16edf1824 (+2 squashed commit)
    
    Squashed commit:
    
    [cd7b04e] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35198 dd3d63e4-b46e-4461-a8c4-23e16edf1824
    
    [e472875] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35197 dd3d63e4-b46e-4461-a8c4-23e16edf1824
    
    [e7a2ce2] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35196 dd3d63e4-b46e-4461-a8c4-23e16edf1824
    
    [cebfba1] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35195 dd3d63e4-b46e-4461-a8c4-23e16edf1824 (+2 squashed commit)
    
    Squashed commit:
    
    [c4a4b33] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35195 dd3d63e4-b46e-4461-a8c4-23e16edf1824
    
    [7fc569f] apoplexy level xml read
    
    git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35213 dd3d63e4-b46e-4461-a8c4-23e16edf1824 (+2 squashed commit)
    
    Squashed commit:
    
    [6208216] delete for error codeplex
    
    git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35212 dd3d63e4-b46e-4461-a8c4-23e16edf1824
    
    [6b59bba] git-svn-id: https://princeofpersia.svn.codeplex.com/svn@35211 dd3d63e4-b46e-4461-a8c4-23e16edf1824

---
+++ PrinceOfPersia.net/PrinceOfPersia.net/animation/Sequence.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace PrinceOfPersia
+{
+    public class Sequence : ICloneable
+    {
+        private const int FRAME_WIDTH = 114;
+        private const int FRAME_HEIGHT = 114;
+        //public const float FRAME_TIME = 0.1f;
+
+        public string config_type;
+
+        public List<Frame> frames = new List<Frame>();
+        public string name;
+        public bool raised = false;
+        public Enumeration.TileType tileType;
+        public Enumeration.TileCollision collision;
+
+        /// <summary>
+        /// Get the Total xOffset and yOffset
+        /// 
+        /// </summary>
+        /// <retur>
+        /// Vector2
+        /// </retur>
+        public Vector2 CountOffSet
+        {
+            get
+            {
+                if (this == null)
+                { return Vector2.Zero; }
+                int x = 0,y=0;
+                foreach (Frame f in frames)
+                { 
+                    x += f.xOffSet;
+                    y += f.yOffSet;
+                }
+                return new Vector2((int)x, (int)y); 
+            }
+        }
+
+        
+        
+        public void Initialize(ContentManager Content)
+        {
+            foreach (Frame f in frames)
+            {
+                try
+                {
+                    //loading texture
+                    if (f.value != null)
+                    {
+						Texture2D t = null;
+#if WINDOWS   
+                        t = (Texture2D)Maze.dContentRes[System.Configuration.ConfigurationSettings.AppSettings[config_type].ToString().ToUpper() + f.value.ToUpper()];
+
+#endif
+#if ANDROID
+
+#endif
+                        if (t == null)
+                            f.SetTexture(null);
+                        else
+                            f.SetTexture(t);
+                    }
+                    //loading sound
+                    if (f.sound != null)
+                    {
+                        SoundEffect s = (SoundEffect)Maze.dContentRes[PrinceOfPersiaGame.CONFIG_SOUNDS + f.sound.ToUpper()];
+                        f.SetSound(s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("ERROR:Content.Load<dContentRes>"+ ex.ToString() + config_type.ToUpper() + f.value); 
+                }
+            }
+        }
+
+        // Deep clone
+        public Sequence DeepClone()
+        {
+            Sequence newSequence = new Sequence();
+            newSequence.name = this.name;
+            newSequence.raised = this.raised;
+            newSequence.collision = this.collision;
+            newSequence.tileType = this.tileType;
+
+            //newSequence.frameTime = this.frameTime;
+            foreach (Frame f in this.frames)
+            {
+                newSequence.frames.Add(f.DeepCopy());
+            }
+            return newSequence;
+
+        }
+
+        
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+        public Sequence Clone()
+        {
+            return (Sequence)this.MemberwiseClone();
+        }
+
+
+
+    }
+}
